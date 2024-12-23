4a0d8cdb8bb9b8793916cecc145e4e8be3a08ae7
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-12-23 10:25:28 +0200

    interface for Todo is created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Input from "./components/Input";
-
+import { Todo } from "./model";
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div>
       <span> Todoify</span>
